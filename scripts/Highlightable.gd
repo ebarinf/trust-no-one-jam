b47fa73c8ac6a10eab222dbbb02592d40b5375f9
ebarinf <eduardo.barrientos.inf@gmail.com>
2026-08-25 02:01:25 -0400

    hover bottles

---
+++ scripts/Highlightable.gd
@@ -0,0 +1,25 @@
+extends Node3D
+class_name Highlightable
+
+@export var outline_material: ShaderMaterial
+
+func _ready():
+	_apply_outline_to_children(self)
+	
+func _apply_outline_to_children(node: Node):
+	for child in node.get_children():
+		if child is MeshInstance3D:
+			for i in range(child.get_surface_override_material_count()):
+				var mat = child.get_active_material(i)
+				if mat:
+					var mat_unique = mat.duplicate()
+					mat_unique.next_pass = outline_material.duplicate()
+					child.set_surface_override_material(i, mat_unique)
+					_outline_instances.append(mat_unique.next_pass)
+		_apply_outline_to_children(child)
+
+var _outline_instances: Array = []
+
+func set_highlighted(is_highlighted: bool):
+	for outline_pass in _outline_instances:
+		outline_pass.set_shader_parameter("outline_enabled", 1.0 if is_highlighted else 0.0)
